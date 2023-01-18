47ce55d40b7ac929d9a034cd4e9885e4d0a26a5d
imabhijee <abhijeetyadavchb@gmail.com>
2023-01-18 15:41:38 +0530

    Updates

---
+++ precache-manifest.275cd022535a67f87fd9ee802f2a8843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e6bda776569bd1bd666",
+    "url": "/textutils-react/static/css/main.047ceb4f.chunk.css"
+  },
+  {
+    "revision": "3e6bda776569bd1bd666",
+    "url": "/textutils-react/static/js/main.3e6bda77.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3e897f2210a78e65956d",
+    "url": "/textutils-react/static/js/runtime~main.3e897f22.js"
+  },
+  {
+    "revision": "680ec9759e510db9f78abce873daf1d0",
+    "url": "/textutils-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.788cd38f0f02c92651a8aced8897cd80.js"
+  "/textutils-react/precache-manifest.275cd022535a67f87fd9ee802f2a8843.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/textutils-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
