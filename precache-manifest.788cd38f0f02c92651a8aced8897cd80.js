1afb9b481553bd142998891938b2fbdc818cbc2a
imabhijee <abhijeetyadavchb@gmail.com>
2023-01-18 14:13:48 +0530

    Updates

---
+++ precache-manifest.788cd38f0f02c92651a8aced8897cd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e6bda776569bd1bd666",
+    "url": "/static/css/main.047ceb4f.chunk.css"
+  },
+  {
+    "revision": "3e6bda776569bd1bd666",
+    "url": "/static/js/main.3e6bda77.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "8dd2649c75f1e465e0a0a04aae3eeb2a",
+    "url": "/index.html"
+  }
+];
